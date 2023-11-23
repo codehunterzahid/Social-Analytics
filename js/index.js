553d6a5d6a8f3a07997450da96fed432d02d41a5
codehunterzahid <codehuntingwithzahid@gmail.com>
2023-11-23 19:16:01 +0500

    hide show buttons added

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    document.getElementById("hide").style.display = "none";
+    document.getElementById("hide1").style.display = "none";
+    
+    document.getElementById("show").style.display = "block";
+    document.getElementById("hide1").innerHTML = "Show"
+  }
+  
+  function myFunction1() {
+    document.getElementById("hide").style.display = "block";
+    document.getElementById("hide1").style.display = "block";
+    
+    document.getElementById("show").style.display = "none";
+    document.getElementById("hide1").innerHTML = "Hide"
+  }
